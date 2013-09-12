755962d423032c9ec3afdde2bb4f8851b0848f35
leileitian <1655909198@qq.com>
2013-09-12 09:23:54 +0800

    Update config.php

--- config.php
+++ config.php
@@ -5,11 +5,8 @@
 $G_ROOT = dirname(__FILE__);
 
 $G_CONFIG["weiXin"] = array(
-	'account' => '2326782922@qq.com',
-	'password' => 'leilei2022236',
-
-	//'account' => 'sheng_sen@hotmail.com',
-	//'password' => 'sheng980711',
+	'account' => '',
+	'password' => '',
 	'cookiePath' => $G_ROOT. '/cache/cookie', // cookie缓存文件路径
 	'webTokenPath' => $G_ROOT. '/cache/webToken', // webToken缓存文件路径
 );
